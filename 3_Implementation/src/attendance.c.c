ee8e67dd2d147bd02397cf74b61725615412c946
stepin104311 <69067336+stepin104311@users.noreply.github.com>
2020-11-13 17:50:02 +0530

    Delete attendance.c.c

--- 3_Implementation/src/attendance.c.c
+++
@@ -1,9 +0,0 @@
-#include<attendance.h>
-int normal(float aggregate,int usual)
-{
-		return usual;
-}
-int cac(float aggregate,int proof,int condonation,int usual,int fine)
-{
-		    return usual+fine;
-}
